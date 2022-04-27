d64a497cf3a203bd2e79f915f382e62eeadb3b71
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-27 17:55:10 -0300

    first commit
    Almost finished; its not doing exactly what question 1 orders.

---
+++ lista9-01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i, vetorA[5],vetorB[5];
+
+        for(i=0; i<5; i++){
+          printf("\n digite  um numero[%d]",i);
+           scanf(" %d", &vetorA[i]);
+        }
+
+       for(i=0; i<5; i++){
+         vetorB[i]=vetorA[i]*2;
+         printf("\n resultdo= %d >> %d \n",i,vetorB[i]);
+
+        }
+  return 0;
+}
